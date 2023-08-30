759c7f078f5076602648bfd998e6406ce7441d89
Daniel <daniel.awaritefe@fatbeehive.com>
2023-08-31 00:41:31 +0100

    created modal state with toggle boolean and set up in header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,7 +3,11 @@
   <header>
     <nav class="md:container md:mx-auto py-6">
       <ul class="flex justify-evenly items-end">
-        <li><a class="text-slate-400 hover:text-slate-200" href="#">Resume</a></li>
+        <li>
+          <a class="text-slate-400 hover:text-slate-200" href="#" @click.prevent="toggleModal"
+            >Resume</a
+          >
+        </li>
         <li><a class="text-slate-400 hover:text-slate-200" href="#">About</a></li>
         <li><span class="font-header text-white text-2xl">Daniel Awaritefe</span></li>
         <li><a class="text-slate-400 hover:text-slate-200" href="#">Portfolio</a></li>
@@ -12,3 +16,20 @@
     </nav>
   </header>
 </template>
+
+<script>
+import { mapStores } from 'pinia'
+import useModalStore from '@/stores/modal'
+export default {
+  name: 'AppHeader',
+  computed: {
+    ...mapStores(useModalStore)
+  },
+  methods: {
+    toggleModal() {
+      this.modalStore.isOpen = !this.modalStore.isOpen
+      console.log(this.modalStore.isOpen)
+    }
+  }
+}
+</script>
