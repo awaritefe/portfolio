48e6217b5977f2cb70c8478417683577c1b1b8ea
Daniel <daniel.awaritefe@fatbeehive.com>
2023-08-30 23:50:48 +0100

    changed es link rules and added more styling to reseme section

--- src/components/AppPortfolio.vue
+++ src/components/AppPortfolio.vue
@@ -3,7 +3,7 @@
   <div class="portfolio grid grid-cols-2 xl:container xl:mx-auto">
     <div class="portfolio-card">
       <picture class="portfolio-image w-auto">
-        <img class="aspect-square object-cover" src="../src/assets/images/pf1.jpg" />
+        <img class="aspect-square object-cover" src="/src/assets/images/pf1.jpg" />
       </picture>
       <div class="portfolio-info">
         <h2 class="text-white">Title of project</h2>
@@ -12,7 +12,7 @@
     </div>
     <div class="portfolio-card">
       <picture class="portfolio-image w-auto">
-        <img class="aspect-square object-cover" src="../src/assets/images/pf2.jpg" />
+        <img class="aspect-square object-cover" src="/src/assets/images/pf2.jpg" />
       </picture>
       <div class="portfolio-info">
         <h2 class="text-white">Title of project</h2>
@@ -21,7 +21,7 @@
     </div>
     <div class="portfolio-card">
       <picture class="portfolio-image w-auto">
-        <img class="aspect-square object-cover" src="../src/assets/images/pf3.jpg" />
+        <img class="aspect-square object-cover" src="/src/assets/images/pf3.jpg" />
       </picture>
       <div class="portfolio-info">
         <h2 class="text-white">Title of project</h2>
@@ -30,7 +30,7 @@
     </div>
     <div class="portfolio-card">
       <picture class="portfolio-image w-auto">
-        <img class="aspect-square object-cover" src="../src/assets/images/pf4.jpg" />
+        <img class="aspect-square object-cover" src="/src/assets/images/pf4.jpg" />
       </picture>
       <div class="portfolio-info">
         <h2 class="text-white">Title of project</h2>
--- src/components/AppReseme.vue
+++ src/components/AppReseme.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
+  <div class="absolute bg-black/70 w-full h-full">
     <iframe
+      class="m-auto"
       src="https://drive.google.com/file/d/1sxLXiU3UrDubPX2V7F2G2fy1MmaExbSG/preview"
       frameBorder="0"
       scrolling="auto"
