44a8427d27af7dd3ff729379cb7b4673c5827c95
Daniel <awaritefe@hotmail.co.uk>
2023-08-31 11:15:07 +0100

    modal getter set and created and binded to class

--- src/components/AppReseme.vue
+++ src/components/AppReseme.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="absolute bg-black/70 w-full h-full">
+  <div id="modal" class="absolute bg-black/70 w-full h-full" :class="hiddenClass">
     <iframe
       class="m-auto"
       src="https://drive.google.com/file/d/1sxLXiU3UrDubPX2V7F2G2fy1MmaExbSG/preview"
@@ -14,7 +14,12 @@
 </template>
 
 <script>
+import { mapState } from 'pinia'
+import useModelStore from '@/stores/modal'
 export default {
-  name: 'AppReseme'
+  name: 'AppReseme',
+  computed: {
+    ...mapState(useModelStore, ['hiddenClass'])
+  }
 }
 </script>
