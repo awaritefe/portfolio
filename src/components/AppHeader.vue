379c030b30379a6ecdfd192d5a2e4f26fe3facaf
awaritefe <awaritefe@hotmail.co.uk>
2024-05-22 21:02:23 +0100

    hover state on header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,25 @@
 <template>
-  <!-- Header -->
   <header>
-    <nav class="md:container md:mx-auto py-6">
+    <nav class="md:container md:mx-auto py-6" ref="headerContainer">
       <ul class="flex justify-evenly items-end">
-        <li>
-          <a class="text-slate-400 hover:text-slate-200" href="#" @click.prevent="toggleModal"
-            >Resume</a
+        <li
+          v-for="(item, index) in headerItems"
+          :key="index"
+          :ref="`listItem_${index}`"
+          @mousemove="applyOverlayMask($event, index)"
+          @mouseleave="resetOverlayMask(index)"
+        >
+          <a
+            :class="{
+              'text-slate-400 hover:text-slate-200': index !== 2,
+              'font-header text-white text-2xl': index === 2
+            }"
+            :href="item.href"
+            @click.prevent="handleClick(index)"
+            >{{ item.label }}</a
           >
+          <div class="overlay" :style="overlayStyles[index]" :ref="`overlay_${index}`"></div>
         </li>
-        <li><a class="text-slate-400 hover:text-slate-200" href="#">About</a></li>
-        <li><span class="font-header text-white text-2xl">Daniel Awaritefe</span></li>
-        <li><a class="text-slate-400 hover:text-slate-200" href="#">Portfolio</a></li>
-        <li><a class="text-slate-400 hover:text-slate-200" href="#">Contact</a></li>
       </ul>
     </nav>
   </header>
@@ -20,8 +28,21 @@
 <script>
 import { mapStores } from 'pinia'
 import useModalStore from '@/stores/modal'
+
 export default {
   name: 'AppHeader',
+  data() {
+    return {
+      overlayStyles: [],
+      headerItems: [
+        { label: 'Resume', href: '#', backgroundColor: 'hsla(165, 82.26%, 51.37%, 0.75)' },
+        { label: 'About', href: '#', backgroundColor: 'hsla(210, 82.26%, 51.37%, 0.75)' },
+        { label: 'Daniel Awaritefe', backgroundColor: 'hsla(255, 82.26%, 51.37%, 0.75)' },
+        { label: 'Portfolio', href: '#', backgroundColor: 'hsla(300, 82.26%, 51.37%, 0.75)' },
+        { label: 'Contact', href: '#', backgroundColor: 'hsla(345, 82.26%, 51.37%, 0.75)' }
+      ]
+    }
+  },
   computed: {
     ...mapStores(useModalStore)
   },
@@ -29,13 +50,82 @@ export default {
     toggleModal() {
       this.modalStore.isOpen = !this.modalStore.isOpen
       console.log(this.modalStore.isOpen)
+    },
+    applyOverlayMask(event, index) {
+      const overlayEl = this.$refs[`overlay_${index}`][0]
+      const listItemEl = this.$refs[`listItem_${index}`][0]
+      const overlayRect = overlayEl.getBoundingClientRect()
+      const listItemRect = listItemEl.getBoundingClientRect()
+      const x = event.clientX - overlayRect.left
+      const y = event.clientY - overlayRect.top
+
+      this.overlayStyles[index] = {
+        opacity: 1,
+        backgroundColor: this.headerItems[index].backgroundColor,
+        '--x': `${x}px`,
+        '--y': `${y}px`,
+        width: `${listItemRect.width}px`, // Set width to match the list item
+        height: `${listItemRect.height}px` // Set height to match the list item
+      }
+    },
+
+    resetOverlayMask(index) {
+      this.overlayStyles[index] = { opacity: 0 }
+    },
+    handleClick(index) {
+      if (index === 0) {
+        this.toggleModal()
+      }
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-li a {
-  @extend %button;
+/* Add your styles here */
+li {
+  position: relative;
+  width: 100%;
+  align-self: stretch;
+  text-align: center;
+  padding: 1rem 0;
+  a {
+    display: grid;
+    height: 100%;
+    align-content: center;
+  }
+}
+
+.overlay {
+  position: absolute;
+  inset: 1;
+  pointer-events: none;
+  user-select: none;
+  opacity: var(--opacity, 0);
+  -webkit-mask: radial-gradient(25rem 25rem at var(--x) var(--y), #000 1%, transparent 50%);
+  mask: radial-gradient(25rem 25rem at var(--x) var(--y), #000 1%, transparent 50%);
+  transition:
+    opacity 0.4s ease,
+    -webkit-mask 0.4s ease,
+    mask 0.4s ease;
+  will-change:
+    opacity,
+    -webkit-mask,
+    mask;
+}
+
+.overlay .item {
+  background-color: hsla(var(--hsl), 0.15);
+  border-color: hsla(var(--hsl), 1);
+  box-shadow: 0 0 0 1px inset hsl(var(--hsl));
+}
+
+li:hover .overlay {
+  opacity: 1;
+  height: 10px !important;
+}
+
+li:hover a {
+  background-color: inherit;
 }
 </style>
